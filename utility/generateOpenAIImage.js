2e7d04cd46f52ca3c5842ee8e7ecda3ff7b1ca13
sandl-pro <lex368417@gmail.com>
2025-01-17 19:38:05 +0800

    Create generateOpenAIImage.js

---
+++ utility/generateOpenAIImage.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: __dirname + '/../.env' })
+var getCleanError = require("./errorHandling.js");
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function grab(query) {
+    try {
+        const response = await openai.createImage({
+            prompt: query,
+            n: 1, // should I raise this
+            size: "1024x1024",
+        });
+        var image_url = response.data.data[0].url;
+
+        return image_url
+    } catch (err) {
+        throw getCleanError.clean(err)
+    }
+}
+
+module.exports.grab = grab;
