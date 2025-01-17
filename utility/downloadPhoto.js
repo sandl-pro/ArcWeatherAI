b2c23cb2de282d9c4202959db489627cf337fe28
sandl-pro <lex368417@gmail.com>
2025-01-17 19:36:15 +0800

    Create downloadPhoto.js

---
+++ utility/downloadPhoto.js
@@ -0,0 +1,52 @@
+require('dotenv').config({ path: __dirname + '/../.env' });
+
+const sharp = require('sharp');
+
+var formatName = require("./nameFormatterTool.js");
+const fs = require('fs');
+
+async function go(url, name, unixTimeStamp, basePWD) {
+    try {
+        const response = await fetch(url);
+        const blob = await response.blob();
+        const arrayBuffer = await blob.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        var formattedName = formatName.format(name, unixTimeStamp);
+        var finalPwd = basePWD + formattedName;
+
+        // update this to be by byte size
+        if (basePWD == "/Users/mikeland/Desktop/Midjourney/") {
+            const image = await sharp(buffer).resize(1024, 1024).toBuffer();
+            console.log("Writing to", finalPwd);
+            await fs.writeFileSync(finalPwd, image);
+        } else {
+            await fs.writeFileSync(finalPwd, buffer);
+        }
+
+        console.log("Photo", finalPwd, "successfully created");
+
+        return formattedName
+    } catch (err) {
+        console.log("Error on downloadPhoto", err);
+        process.exit(1);
+    };
+}
+
+async function downloadCensoredPhoto(censorFileName, unixTimeStamp, basePWD) {
+    try {
+        console.log("Downloading Censored Photo", censorFileName);
+
+        var formattedName = formatName.format(censorFileName, unixTimeStamp);
+        await fs.copyFileSync("/Users/mikeland/censoredImages/" + censorFileName + ".png", basePWD + formattedName);
+        console.log("Successfully Downloaded Censored Photo", formattedName);
+
+        return formattedName
+    } catch (err) {
+        console.log("Error on downloadCensoredPhoto", err);
+        process.exit(1);
+    }
+};
+
+module.exports.go = go;
+module.exports.downloadCensoredPhoto = downloadCensoredPhoto;
