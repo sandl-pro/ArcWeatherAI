5deb0c72135cc7b414275a06c0ed35491a51f0f5
sandl-pro <lex368417@gmail.com>
2025-01-17 19:37:45 +0800

    Create genHTML.js

---
+++ utility/genHTML.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+async function gen(pkg) {
+
+    try {
+        const html = `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Weather Window</title>
+            </head>
+            <link rel="stylesheet" type="text/css" href="style.css">
+            <body>
+                <div class="container" >
+                    <img src=${pkg.photoPWD} >
+                    <h1>${pkg.source} Prompt: "${pkg.query}"</h1>
+                </div>
+            </body>
+        </html>
+        `;
+
+        await fs.promises.writeFile(__dirname+'/../home.html', html);
+        
+    } catch (err) {
+        console.log("Error on genHTML", err);
+        throw err
+    }
+}
+
+module.exports.gen = gen;
