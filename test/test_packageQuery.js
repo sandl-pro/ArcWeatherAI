a56216b8b612ba37964828771807209d9f8593ad
sandl-pro <lex368417@gmail.com>
2025-01-17 19:33:27 +0800

    test_packageQuery.js

---
+++ test/test_packageQuery.js
@@ -0,0 +1,74 @@
+var assert = require('assert');
+var packageQuery = require("../utility/packageQuery.js");
+
+describe("Testing cleaning the Place Query", function() {
+    it('With São Gonçalo Brazil should turn to Sao Goncalo Brazil', function() {
+        var query = "São Gonçalo Brazil";
+        var cleanQuery = packageQuery.clean(query);
+
+        assert.equal(cleanQuery, "Sao Goncalo Brazil")
+    })
+
+    it('With Alīgarh India should turn to Aligarh India', function() {
+        var query = "Alīgarh India";
+        var cleanQuery = packageQuery.clean(query);
+
+        assert.equal(cleanQuery, "Aligarh India")
+    })
+
+    it('With Sūrat India should turn to Surat India', function() {
+        var query = "Sūrat India";
+        var cleanQuery = packageQuery.clean(query);
+
+        assert.equal(cleanQuery, "Surat India")
+    })
+
+    it('With Biên Hòa Vietnam should turn to Bien Hoa Vietnam', function() {
+        var query = "Biên Hòa Vietnam";
+        var cleanQuery = packageQuery.clean(query);
+
+        assert.equal(cleanQuery, "Bien Hoa Vietnam")
+    })
+
+    it('With Guwāhāti India should turn to Guwahati India', function() {
+        var query = "Guwāhāti India";
+        var cleanQuery = packageQuery.clean(query);
+
+        assert.equal(cleanQuery, "Guwahati India")
+    })
+
+    it('With Yan’an China should turn to Yanan China', function() {
+        var query = "Yan’an China";
+        var cleanQuery = packageQuery.clean(query);
+
+        assert.equal(cleanQuery, "Yanan China")
+    })
+
+    it('With Ürümqi China should turn to Urumqi China', function() {
+        var query = "Ürümqi China";
+        var cleanQuery = packageQuery.clean(query);
+
+        assert.equal(cleanQuery, "Urumqi China")
+    })
+
+    it('With Ghāziābād India should turn to Ghaziabad India', function() {
+        var query = "Ghāziābād India";
+        var cleanQuery = packageQuery.clean(query);
+
+        assert.equal(cleanQuery, "Ghaziabad India")
+    })
+
+    it('With Ōsaka Japan should turn to Osaka Japan', function() {
+        var query = "Ōsaka Japan";
+        var cleanQuery = packageQuery.clean(query);
+
+        assert.equal(cleanQuery, "Osaka Japan")
+    })
+
+    it('With Kyōto Japan should turn to Kyoto Japan', function() {
+        var query = "Kyōto Japan";
+        var cleanQuery = packageQuery.clean(query);
+
+        assert.equal(cleanQuery, "Kyoto Japan")
+    })
+})
