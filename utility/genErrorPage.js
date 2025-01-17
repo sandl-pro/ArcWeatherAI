49a47ec08aaeb5fcbaf247b34cc4cc4f949057a2
sandl-pro <lex368417@gmail.com>
2025-01-17 19:37:19 +0800

    Create genErrorPage.js

---
+++ utility/genErrorPage.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+async function gen(err) {
+    try {
+        console.log("Generating Error Page");
+        const html = `
+        <!DOCTYPE html>
+        <html>
+            <body>
+                <center>
+                    <h1>ERROR<h1>
+                    <h1>${err}</h1>
+                </center>
+            </body>
+        </html>
+        `;
+
+        await fs.promises.writeFile(__dirname+'/../home.html', html);
+        console.log("Successfully Generated Error Page at home.html");
+        process.exit(1);
+    } catch (err) {
+        console.log("ERROR genErrorPage", err);
+        process.exit(1);
+    }
+}
+
+module.exports.gen = gen;
